8b4f133454901ce398fa45c3f6dbe0596a357b12
nefzina <nefzi.amani@gmail.com>
2023-12-21 10:28:18 +0100

    spring dependencies & DDD directories

---
+++ src/main/java/com/wildcodeschool/webook/WebookApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.webook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebookApplication.class, args);
+	}
+
+}
