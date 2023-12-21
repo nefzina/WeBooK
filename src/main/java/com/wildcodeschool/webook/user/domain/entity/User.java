170be29417e39c6bc872ce159b07ab3ff7427baf
nefzina <nefzi.amani@gmail.com>
2023-12-21 15:21:57 +0100

    class user including entity, repo, controller & service, TODO: foreign keys & login, registration and auth

---
+++ src/main/java/com/wildcodeschool/webook/user/domain/entity/User.java
@@ -0,0 +1,66 @@
+package com.wildcodeschool.webook.user.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "zipCode", nullable = true)
+    private Number zip_code;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Number getZip_code() {
+        return zip_code;
+    }
+
+    public void setZip_code(Number zip_code) {
+        this.zip_code = zip_code;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
---
+++ src/main/java/com/wildcodeschool/webook/user/infrastructure/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.wildcodeschool.webook.user.infrastructure.repository;
+
+import com.wildcodeschool.webook.user.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
