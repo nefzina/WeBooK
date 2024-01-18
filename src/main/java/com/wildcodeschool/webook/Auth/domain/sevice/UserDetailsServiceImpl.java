09b1d8ecfdbee311128855ec7cf90180950acdcd
nefzina <nefzi.amani@gmail.com>
2024-01-18 16:36:03 +0100

    fix userDetailsServiceImpl error, timeZone error

--- src/main/java/com/wildcodeschool/webook/Auth/domain/sevice/UserDetailsServiceImpl.java
+++ src/main/java/com/wildcodeschool/webook/Auth/domain/sevice/UserDetailsServiceImpl.java
@@ -3,11 +3,13 @@ package com.wildcodeschool.webook.Auth.domain.sevice;
 import com.wildcodeschool.webook.Auth.domain.dto.UserPrincipal;
 import com.wildcodeschool.webook.book.infrastructure.exception.NotFoundException;
 import com.wildcodeschool.webook.Auth.infrastructure.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public abstract class UserDetailsServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
     public UserDetailsServiceImpl(UserRepository userRepository){
@@ -15,5 +17,10 @@ public abstract class UserDetailsServiceImpl implements UserDetailsService {
     }
     public UserPrincipal loadUserByEmail(String email) throws NotFoundException {
         return new UserPrincipal(userRepository.findByEmail(email));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
     }
 }
